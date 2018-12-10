f62bd3c7e25d2fa9a19b0fd31871f0b51b315ce0
李理 <hareis@163.com>
2018-12-10 17:05:20 +0800

    init

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func main() {
+	address := flag.String("a", "127.0.0.1", "address")
+	port := flag.Int("p", 8080, "port")
+	flag.Parse()
+
+	httpAddress := *address + ":" + strconv.Itoa(*port)
+
+	srv := &http.Server{Addr: httpAddress}
+
+	dirPath, _ := os.Getwd()
+	fmt.Println("Work dir:", dirPath)
+	http.Handle("/", http.FileServer(http.Dir(dirPath)))
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			fmt.Printf("Httpserver: ListenAndServe() error: %s", err)
+		}
+	}()
+	fmt.Println("created httpServer success!")
+	fmt.Println("server listening at: http://" + httpAddress)
+
+	fmt.Println("if you want to stop the server, please press any key !")
+
+	var command string
+	fmt.Scanf("%s", &command)
+
+	if err := srv.Shutdown(nil); err != nil {
+		panic(err) // failure/timeout shutting down the server gracefully
+	}
+	fmt.Println("httpServer stop!")
+}
